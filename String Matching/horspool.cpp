ad9d3bb16c278cdd79f22098f33ed2d03e82ab4c
xf <37798958+githuberiex@users.noreply.github.com>
2020-12-17 14:19:44 +0800

    Update horspool.cpp

--- String Matching/horspool.cpp
+++ String Matching/horspool.cpp
@@ -6,9 +6,11 @@ int horspool(string s, string p) {
     int len_p = p.size(); //模式长度
     int i = 0;
     int count = 1;
+    
     while (len_s > 0 && len_p > 0 && i <= (len_s - len_p)) {
         int k;
         int j = len_p - 1;
+        
         while (j >= 0 && p[j] == s[i + j]) // p与s从右往左匹配
             j--;
         if (j == -1) //匹配成功，返回对应位置i
@@ -22,9 +24,9 @@ int horspool(string s, string p) {
                     break;
                 }
             if (flag)          // p前len_p-1中出现s[i+j]这个元素
-                i = i + j - k; //右移j-k长度
+                i = i + j - k; // 右移j-k长度
             else               // p前len_p-1中未现s[i+j]这个元素
-                i += len_p;    //右移len_p长度
+                i += len_p;    // 右移len_p长度
         }
         count++;
     }
